457d702fe1fbb8546d2a6acc6f188208405c0e4f
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-01-25 23:37:55 +0100

    Setup timber

---
+++ app/src/main/java/com/example/instantweather/InstantWeatherApplication.kt
@@ -0,0 +1,14 @@
+package com.example.instantweather
+
+import android.app.Application
+import timber.log.Timber
+
+/**
+ * Created by Mayokun Adeniyi on 2020-01-25.
+ */
+class InstantWeatherApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant()
+    }
+}
