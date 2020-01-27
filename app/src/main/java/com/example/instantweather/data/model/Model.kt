ed02d9dc82fb7abdd67d466e09fc224e897d8109
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-01-28 00:39:46 +0100

    Create dto converter

--- app/src/main/java/com/example/instantweather/data/model/Model.kt
+++ app/src/main/java/com/example/instantweather/data/model/Model.kt
@@ -1,31 +1,47 @@
 package com.example.instantweather.data.model
 
+import com.example.instantweather.data.local.entity.WeatherResponse
 import com.google.gson.annotations.SerializedName
 
 /**
  * Created by Mayokun Adeniyi on 2020-01-25.
  */
 
-class CityWeather(
-    val id: Long,
+class CityWeatherDto(
+
+    val uId: Long,
+
+    @SerializedName("id")
+    val cityId: Long,
+
     val name: String,
 
     @SerializedName("weather")
-    val weathers: List<Weather>,
+    val weatherDtos: List<WeatherDto>,
 
-    val main: MainOb
+    val main: MainDto
 )
 
 
-class MainOb(
+class MainDto(
     val temp: Double,
     val pressure: Double,
     val humidity: Double
 )
 
-class Weather(
+class WeatherDto(
     val id: Long,
     val main: String?,
     val description: String?,
     val icon: String?
 )
+
+
+fun CityWeatherDto.toDatabaseModel(): WeatherResponse{
+    return WeatherResponse(
+        cityId = this.cityId,
+        cityName = this.name,
+        weatherDto = this.weatherDtos,
+        mainDto = this.main
+    )
+}
