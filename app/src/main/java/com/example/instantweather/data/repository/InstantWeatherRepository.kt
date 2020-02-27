998d23c54d0b129e5ca17aa663693ea34b37bddd
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-02-27 14:19:54 +0100

    Create data repository directory

---
+++ app/src/main/java/com/example/instantweather/data/repository/InstantWeatherRepository.kt
@@ -0,0 +1,7 @@
+package com.example.instantweather.data.repository
+
+/**
+ * Created by Mayokun Adeniyi on 27/02/2020.
+ */
+interface InstantWeatherRepository {
+}
---
+++ app/src/main/java/com/example/instantweather/data/repository/InstantWeatherRepositoryImpl.kt
@@ -0,0 +1,5 @@
+package com.example.instantweather.data.repository
+
+/**
+ * Created by Mayokun Adeniyi on 27/02/2020.
+ */
