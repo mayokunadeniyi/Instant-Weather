937606ab1b371e7aa2653b2a0f633dcafb55eb72
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-03-15 20:34:57 +0100

    Create mapper for weather forecast

---
+++ app/src/main/java/com/example/instantweather/mapper/WeatherForecastMapperRemote.kt
@@ -0,0 +1,37 @@
+package com.example.instantweather.mapper
+
+import com.example.instantweather.data.model.NetworkWeatherForecast
+import com.example.instantweather.data.model.WeatherForecast
+
+/**
+ * Created by Mayokun Adeniyi on 15/03/2020.
+ */
+
+class WeatherForecastMapperRemote :
+    BaseMapper<List<NetworkWeatherForecast>, List<WeatherForecast>> {
+    override fun transformToDomain(type: List<NetworkWeatherForecast>): List<WeatherForecast> {
+        return type.map { networkWeatherForecast ->
+            WeatherForecast(
+                networkWeatherForecast.id,
+                networkWeatherForecast.date,
+                networkWeatherForecast.wind,
+                networkWeatherForecast.networkWeatherDescription,
+                networkWeatherForecast.networkWeatherCondition
+            )
+        }
+    }
+
+    override fun transformToDto(type: List<WeatherForecast>): List<NetworkWeatherForecast> {
+        return type.map { weatherForecast ->
+            NetworkWeatherForecast(
+                weatherForecast.uID,
+                weatherForecast.date,
+                weatherForecast.wind,
+                weatherForecast.networkWeatherDescription,
+                weatherForecast.networkWeatherCondition
+            )
+        }
+    }
+
+
+}
