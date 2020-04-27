1b008f425fcde8c7d5c7fdf737d58eff163e7a7f
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-04-27 20:58:57 +0100

    Feat: create search fragment

---
+++ app/src/main/java/com/example/instantweather/ui/search/SearchFragmentViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.instantweather.ui.search
+
+import android.app.Application
+import com.example.instantweather.ui.BaseViewModel
+
+/**
+ * Created by Mayokun Adeniyi on 27/04/2020.
+ */
+
+class SearchFragmentViewModel private constructor(application: Application):
+        BaseViewModel(application){
+
+}
