6b5999aa0303364f0ee8879a78e9c1d2d53a420f
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-05-01 20:52:42 +0100

    Feat: create search fragment

--- app/src/main/java/com/example/instantweather/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/instantweather/ui/search/SearchFragment.kt
@@ -3,22 +3,112 @@ package com.example.instantweather.ui.search
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.autofill.AutofillValue
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.algolia.instantsearch.core.connection.ConnectionHandler
+import com.algolia.instantsearch.helper.android.item.StatsTextView
+import com.algolia.instantsearch.helper.android.searchbox.SearchBoxViewAppCompat
+import com.algolia.instantsearch.helper.android.searchbox.connectView
+import com.algolia.instantsearch.helper.stats.StatsPresenterImpl
+import com.algolia.instantsearch.helper.stats.connectView
 
 import com.example.instantweather.R
+import com.example.instantweather.databinding.FragmentSearchBinding
+import com.example.instantweather.ui.search.SearchResultAdapter.SearchResultListener
+import com.example.instantweather.utils.SharedPreferenceHelper
+import com.example.instantweather.utils.showIf
+import com.google.android.material.snackbar.Snackbar
+import timber.log.Timber
 
 /**
  * A simple [Fragment] subclass.
  */
 class SearchFragment : Fragment() {
+    private lateinit var binding: FragmentSearchBinding
+    private lateinit var viewModel: SearchFragmentViewModel
+    private lateinit var searchResultAdapter: SearchResultAdapter
+    private val connection = ConnectionHandler()
+    private lateinit var searchBoxView: SearchBoxViewAppCompat
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        binding = FragmentSearchBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(SearchFragmentViewModel::class.java)
+
+        searchBoxView = SearchBoxViewAppCompat(binding.searchView)
+        searchBoxView.searchView.isIconified = false
+
+        val statsView = StatsTextView(binding.stats)
+        connection += viewModel.searchBox.connectView(searchBoxView)
+        connection += viewModel.stats.connectView(statsView, StatsPresenterImpl())
+
+        searchResultAdapter = SearchResultAdapter(SearchResultListener { name ->
+            searchBoxView.setText(name)
+            viewModel.getSearchWeather(name)
+            observeViewModel(name)
+        })
+
+        searchBoxView.onQuerySubmitted = {
+            if (it != null && it.isNotEmpty()){
+                viewModel.getSearchWeather(it)
+                observeViewModel(it)
+            }
+        }
+
+
+        val recyclerView = binding.locationSearchRecyclerview
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = searchResultAdapter
+
+        viewModel.locations.observe(viewLifecycleOwner, Observer { hits ->
+            searchResultAdapter.submitList(hits)
+            binding.zeroHits.showIf { hits.size == 0 }
+        })
+
+    }
+
+    private fun observeViewModel(location: String) {
+        viewModel.searchWeather.observe(viewLifecycleOwner, Observer { weather ->
+            if (weather != null){
+                val action = SearchFragmentDirections.actionSearchFragmentToSearchDetailFragment(weather,location)
+                findNavController().navigate(action)
+            }
+        })
+
+        viewModel.isLoading.observe(viewLifecycleOwner, Observer { state ->
+            binding.searchWeatherLoader.showIf { state }
+        })
+
+        viewModel.searchWeatherState.observe(viewLifecycleOwner, Observer { state ->
+            if (!state){
+               Snackbar.make(requireView(),"An error occured! Try again",Snackbar.LENGTH_LONG).show()
+            }
+        })
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        connection.disconnect()
     }
 
 }
--- app/src/main/java/com/example/instantweather/ui/search/SearchFragmentViewModel.kt
+++ app/src/main/java/com/example/instantweather/ui/search/SearchFragmentViewModel.kt
@@ -1,13 +1,81 @@
 package com.example.instantweather.ui.search
 
 import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.algolia.instantsearch.core.connection.ConnectionHandler
+import com.algolia.instantsearch.helper.android.list.SearcherSingleIndexDataSource
+import com.algolia.instantsearch.helper.android.searchbox.SearchBoxConnectorPagedList
+import com.algolia.instantsearch.helper.searcher.SearcherSingleIndex
+import com.algolia.instantsearch.helper.stats.StatsConnector
+import com.algolia.search.client.ClientSearch
+import com.algolia.search.model.APIKey
+import com.algolia.search.model.ApplicationID
+import com.algolia.search.model.IndexName
+import com.example.instantweather.BuildConfig
+import com.example.instantweather.data.local.WeatherDatabase
+import com.example.instantweather.data.model.SearchResult
+import com.example.instantweather.data.model.Weather
+import com.example.instantweather.data.repository.InstantWeatherRepository
 import com.example.instantweather.ui.BaseViewModel
+import com.example.instantweather.utils.ALGOLIA_INDEX_NAME
+import io.ktor.client.features.logging.LogLevel
 
 /**
  * Created by Mayokun Adeniyi on 27/04/2020.
  */
 
-class SearchFragmentViewModel private constructor(application: Application):
-        BaseViewModel(application){
+class SearchFragmentViewModel(application: Application) :
+    BaseViewModel(application) {
+
+    private val database = WeatherDatabase.getInstance(getApplication())
+    private var repository: InstantWeatherRepository
+    private val applicationID = BuildConfig.ALGOLIA_APP_ID
+    private val algoliaAPIKey = BuildConfig.ALGOLIA_API_KEY
+    private val client = ClientSearch(
+        ApplicationID(applicationID),
+        APIKey(algoliaAPIKey)
+    )
+    private val index = client.initIndex(IndexName(ALGOLIA_INDEX_NAME))
+    private val searcher = SearcherSingleIndex(index)
+
+    private val dataSourceFactory = SearcherSingleIndexDataSource.Factory(searcher) { hit ->
+        SearchResult(
+            name = hit.json.getPrimitive("name").content,
+            subcountry = hit.json.getPrimitive("subcountry").content,
+            country = hit.json.getPrimitive("country").content
+        )
+    }
+
+    private val pagedListConfig = PagedList.Config.Builder().setPageSize(50).build()
+    val locations: LiveData<PagedList<SearchResult>> =
+        LivePagedListBuilder(dataSourceFactory, pagedListConfig).build()
+
+    val searchBox = SearchBoxConnectorPagedList(searcher, listOf(locations))
+    val stats = StatsConnector(searcher)
+    private val connection = ConnectionHandler()
+
+    init {
+        repository = InstantWeatherRepository(database,application)
+        connection += searchBox
+        connection += stats
+    }
+
+    val searchWeather: LiveData<Weather> = repository.searchWeather
+    val isLoading: LiveData<Boolean> = repository.searchWeatherIsLoading
+    val searchWeatherState: LiveData<Boolean> = repository.searchWeatherState
+
+    fun getSearchWeather(name: String){
+        repository.getSearchRemoteWeather(name)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+        searcher.cancel()
+        connection.disconnect()
+    }
+
 
 }
