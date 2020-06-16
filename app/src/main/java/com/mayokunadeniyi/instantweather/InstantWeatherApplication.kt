8151316f440a3eecb734e999b3f6a6789063e4c3
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-06-16 14:23:11 +0100

    Custom workmanager init

--- app/src/main/java/com/mayokunadeniyi/instantweather/InstantWeatherApplication.kt
+++ app/src/main/java/com/mayokunadeniyi/instantweather/InstantWeatherApplication.kt
@@ -1,14 +1,18 @@
 package com.mayokunadeniyi.instantweather
 
 import android.app.Application
+import android.util.Log
 import androidx.preference.PreferenceManager
+import androidx.work.Configuration
+import androidx.work.DelegatingWorkerFactory
 import com.mayokunadeniyi.instantweather.utils.ThemeManager
+import com.mayokunadeniyi.instantweather.worker.MyWorkerFactory
 import timber.log.Timber
 
 /**
  * Created by Mayokun Adeniyi on 2020-01-25.
  */
-class InstantWeatherApplication: Application() {
+class InstantWeatherApplication: Application(), Configuration.Provider {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
@@ -18,5 +22,16 @@ class InstantWeatherApplication: Application() {
     private fun initTheme(){
         val preferences = PreferenceManager.getDefaultSharedPreferences(this)
         ThemeManager.applyTheme(preferences.getString("theme_key","")!!)
+    }
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        val myWorkerFactory = DelegatingWorkerFactory()
+        myWorkerFactory.addFactory(MyWorkerFactory(this))
+        // Add here other factories that you may need in this application
+
+        return Configuration.Builder()
+            .setMinimumLoggingLevel(Log.INFO)
+            .setWorkerFactory(myWorkerFactory)
+            .build()
     }
 }
