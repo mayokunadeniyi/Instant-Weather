a432a809cdcd5f56e0ce750cb7888efa072c23d7
GeorgCantor <cupsman1986@gmail.com>
2020-07-13 15:16:19 +0300

    fixed packageName in test

--- app/src/androidTest/java/com/mayokunadeniyi/instantweather/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mayokunadeniyi/instantweather/ExampleInstrumentedTest.kt
@@ -19,7 +19,7 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.instantweather", appContext.packageName)
+        assertEquals("com.mayokunadeniyi.instantweather", appContext.packageName)
 
     }
 }
