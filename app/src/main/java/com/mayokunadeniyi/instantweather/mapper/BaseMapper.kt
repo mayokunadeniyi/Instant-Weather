af8ecacb849c76c8161e29341354dd6115b01788
mayokunthefirst <adeniyimayokun17@gmail.com>
2020-08-01 22:27:55 +0100

    Test: delete database test

--- app/src/main/java/com/mayokunadeniyi/instantweather/mapper/BaseMapper.kt
+++ app/src/main/java/com/mayokunadeniyi/instantweather/mapper/BaseMapper.kt
@@ -3,11 +3,10 @@ package com.mayokunadeniyi.instantweather.mapper
 /**
  * Created by Mayokun Adeniyi on 10/03/2020.
  */
+
 interface BaseMapper<E, D> {
 
-    // Transforms a type to a Domain
     fun transformToDomain(type: E): D
 
-    // Transforms a type to a DTO
     fun transformToDto(type: D): E
 }
