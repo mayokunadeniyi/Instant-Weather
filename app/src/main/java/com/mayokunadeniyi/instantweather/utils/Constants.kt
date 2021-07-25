9d4fef0f801924b1564609e85a66070044199d9a
Adeniyi Mayokun <adeniyimayokun17@gmail.com>
2021-07-25 14:21:11 +0100

    Update README.md (#37)
    * chore: manage dependencies with buildSrc
    
    * chore: setup firebase crashlytics, analytics
    
    * chore: convert build scripts to kotlin dsl
    
    * chore: convert build scripts to kotlin dsl
    
    * chore: remove kotlin android extensions, enable viewbinding
    
    * feat: use bottom sheet in search fragmenrt, format forecast dates
    
    * chore: fix build config issues
    
    * refactor: update .gitignore
    
    * refactor: update .gitignore
    
    * ci: update actions workflow
    
    * refactor: fix failing tests
    
    * chore: fix build config issues
    
    * ci: update actions workflow
    
    * refactor: ktlint format
    
    * chore: fix build config issues
    
    * chore: fix build config issues
    
    * chore: fix build config issues
    
    * refactor: update .gitignore
    
    * refactor: ktlint format
    
    * refactor: move algolia index name to build config
    
    * refactor: move algolia index name to build config
    
    * Update README.md
    
    * ci: update actions workflow
    
    * ci: update actions workflow
    
    * ci: update actions workflow

--- app/src/main/java/com/mayokunadeniyi/instantweather/utils/Constants.kt
+++ app/src/main/java/com/mayokunadeniyi/instantweather/utils/Constants.kt
@@ -1,15 +1,7 @@
 package com.mayokunadeniyi.instantweather.utils
 
-import com.mayokunadeniyi.instantweather.BuildConfig
-
 /**
  * Created by Mayokun Adeniyi on 18/03/2020.
  */
 
 const val GPS_REQUEST_CHECK_SETTINGS = 102
-const val ALGOLIA_INDEX_NAME = "prod_LOCATIONS_V2"
-const val API_KEY = BuildConfig.API_KEY
-
-// Used for the LocationLiveData
-const val LOCATION_LONGITUDE = "LOCATION_LONGITUDE"
-const val LOCATION_LATITUDE = "LOCATION_LATITUDE"
